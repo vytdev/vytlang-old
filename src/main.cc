0f4fe4793aa7982bebe3280900316e9c5eb20efe
vytdev <123163428+vytdev@users.noreply.github.com>
2023-09-03 15:17:34 +0800

    some src files

---
+++ src/main.cc
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2023 VYT. All rights reserved.
+//
+// This project is distributed and available under the terms of MIT License
+// See LICENSE for more information
+//
+
+// The version name of this project
+#define VYT_VERSION "0.1.0"
+
+#include <iostream>
+
+#include "../include/main.h"
+
+int main(int argc, char **argv) {
+    // just show version
+    std::cout << VYT_VERSION << std::endl;
+    return 0;
+}
